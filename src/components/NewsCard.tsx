9973412f36e139a6536a41a09dca1d37f0c5beb8
Tanujaya CS <tan.stronaut@gmail.com>
2025-05-02 01:16:52 +0700

    fetch img

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -23,14 +23,18 @@ const NewsCard = ({ news }: NewsCardProps) => {
     <div className="border p-4 rounded-lg shadow-md bg-white">
       {news.gambar && (
         <img
-        src={`https://drive.usercontent.google.com/download?id=${news.gambar}`}
+        src={`https://drive.google.com/thumbnail?id=${news.gambar}`}
           alt={news.judul}
-          className="w-full h-10 object-cover rounded-md mb-4"
+          className="w-full h-100 object-cover rounded-md mb-4"
         />
       )}
 
       <h2 className="text-xl font-semibold mb-2">{news.judul}</h2>
       <p className="text-gray-600 text-sm mb-4">By {news.penulis} - {news.kategori}</p>
+      <p className="text-gray-600 text-sm mb-4">{news.created_at}</p>
+      <p className="text-gray-600 text-sm mb-4">{news.gambar}</p>
+      <p className="text-gray-600 text-sm mb-4">{news.id}</p>
+      <p className="text-gray-600 text-sm mb-4">{news.id}</p>
       <p className="text-gray-800">
         {truncateTo20Sentences(news.deskripsi)}
       </p>
